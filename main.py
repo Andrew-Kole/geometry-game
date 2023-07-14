63bd55fce459900b0f09c06b8f9576ad4dcf7825
pakitowka <whitesaint12@gmail.com>
2023-07-14 18:14:29 +0200

    counting if point in rectangle

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    # Method, that counts if given coordinates are in rectangle
+    def falls_in_rectangle(self, lowleft, upright):
+        if lowleft[0] < self.x < upright[0] and lowleft[1] < self.x < upright[1]:
+            return True
+        else:
+            return False
