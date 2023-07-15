5bbf47f01d9822fb453d4f16de7dd1f986e5582e
pakitowka <whitesaint12@gmail.com>
2023-07-15 18:21:04 +0200

    rectangle class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
+from random import randint
 
 
 # Class to define point
@@ -10,8 +11,9 @@ class Point:
         self.y = y
 
     # Method, that counts if given coordinates are in rectangle
-    def falls_in_rectangle(self, lowleft, upright):
-        if lowleft[0] < self.x < upright[0] and lowleft[1] < self.x < upright[1]:
+    def falls_in_rectangle(self, rectangle):
+        if rectangle.lowleft.x < self.x < rectangle.upright.x\
+                and rectangle.lowleft.y < self.x < rectangle.upright.y:
             return True
         else:
             return False
@@ -19,3 +21,16 @@ class Point:
     # Method, that counts distance from current coordinates to any other point
     def distance_from_point(self, point):
         return math.sqrt(math.pow(self.x - point.x, 2) + math.pow(self.y - point.y, 2))
+
+
+# class that gets left lower point and right upper point and draws rectangle
+class Rectangle:
+
+    def __init__(self, lowleft, upright):
+        self.lowleft = lowleft
+        self.upright = upright
+
+
+rectangle = Rectangle(Point(randint(0, 9), randint(0, 9)),
+                      Point(randint(10, 19), randint(10, 19)))
+
